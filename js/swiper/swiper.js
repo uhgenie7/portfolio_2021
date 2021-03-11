8c62195ba195efb34d197520bc2325d75cdc3b00
hyejin <dzzd777@naver.com>
2021-03-11 18:37:59 +0900

    UPDATE: 스와이퍼 반응형 작업

--- js/swiper/swiper.js
+++ js/swiper/swiper.js
@@ -11,5 +11,12 @@ let swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination',
     clickable : true
   },
+  breakpoints: { // 화면의 넓이가 320px 이상일 때 
+    320: { slidesPerView: 1},
+    // 화면의 넓이가 640px 이상일 때 
+    640: { slidesPerView: 2},
+    1800: { slidesPerView: 3}
+  },
+    
   keyboard: true,
 });
