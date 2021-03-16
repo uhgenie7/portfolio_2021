b44525555652a37cb299bdb101f6503e40ebc9c8
hyejin <dzzd777@naver.com>
2021-03-16 18:38:18 +0900

    UPDATE: 리사이즈 이벤트
    너비에 따라 skills__left 높이 변경

--- js/fullpage/Initialization.js
+++ js/fullpage/Initialization.js
@@ -1,11 +1,13 @@
 
 $("#fullpage").fullpage({
-          anchors:['home', 'skills', 'works', 'contact'],
+          anchors: ['home', 'skills', 'works', 'contact'],
+          menu: '#myMenu',
           verticalCentered: false,
           css3: true,
           navigation: true,
           navigationPosition: "right",
           scrollOverflow: true,
+          responsiveWidth: 1100,
           onLeave: function (index, nextIndex, direction) {
             if (index == 3 && direction == "down") {
               $(".section")
@@ -30,4 +32,28 @@ $("#fullpage").fullpage({
               index == 4 && direction == "up"
             );
           },
-        });
+});
+        
+// skills 의 높이 = 해당 섹션의 높이.
+const skillSection = document.querySelector('.skill__section');
+
+const skillLeft = document.querySelector('.skills__left');
+let skillSectionInfo = skillSection.getBoundingClientRect();
+let skillSectionHeight = skillSectionInfo.height + 20;
+skillLeft.style.height = skillSectionHeight + "px";
+
+
+function handleResize() {
+  let width = window.innerWidth;
+  if (width > 1100) {
+    skillLeft.style.height = skillSectionHeight + 20 + "px";
+  } else if (width < 480){
+    skillLeft.style.height = 300 + "px";
+  } else {
+    skillLeft.style.height = skillSectionHeight / 3 + "px";
+  }
+}
+
+
+handleResize();
+window.addEventListener("resize", handleResize);
